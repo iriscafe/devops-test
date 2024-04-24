7de135f366865607e1571aab451a377eef110a8f
Iris <iriscafe@tmaior.com.br>
2024-04-23 23:24:49 -0300

    updating yaml prometheus yaml and removing test folder from helm charts

--- infra/modules/argocd/argocd.tf
+++ infra/modules/argocd/argocd.tf
@@ -173,27 +173,42 @@ YAML
 
 resource "kubectl_manifest" "prometheus" {
   yaml_body = <<YAML
-project: default
-source:
-  repoURL: 'git@github.com:iriscafe/devops-test.git'
-  path: helm/prometheus
-  targetRevision: HEAD
-  helm:
-    valueFiles:
-      - values.yaml
-destination:
-  server: 'https://kubernetes.default.svc'
-  namespace: monitoring
-syncPolicy:
-  automated:
-    prune: true
-    selfHeal: true
-  syncOptions:
-    - CreateNamespace=true
-    - PruneLast=true
-
-YAML
-
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: prometheus
+  namespace: argocd
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+spec:
+  destination:
+    namespace: monitoring
+    server: "https://kubernetes.default.svc"
+  source:
+    path: "helm/prometheus"
+    repoURL: "git@github.com:iriscafe/devops-test.git"
+    targetRevision: "HEAD"
+    helm:
+      valueFiles:
+        - "values.yaml"
+  project: "default"
+  syncPolicy:
+    managedNamespaceMetadata:
+      labels:
+        managed: "argo-cd"
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+      - PruneLast=true
+  retry:
+    limit: 5
+    backoff:
+      duration: 5s
+      maxDuration: 3m0s
+      factor: 2
+YAML 
 }
 
 resource "kubectl_manifest" "kube_metrics" {
@@ -201,7 +216,7 @@ resource "kubectl_manifest" "kube_metrics" {
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: kube_metrics
+  name: kube-metrics
   namespace: argocd
   finalizers:
     - resources-finalizer.argocd.argoproj.io
